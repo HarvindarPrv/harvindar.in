ad271e372bace3a40b424debc16a08ef13079838
Harvindar Singh <sjagjeet994@gmail.com>
2024-06-11 17:00:02 +0530

    Experience update

---
+++ test.js
@@ -0,0 +1,12 @@
+{
+    "logo": "./data/experience/royalEnfield1.png",
+        "name": "Royal Enfield",
+            "description": "<span class='imp mb-10'>Profile : Workshop Manager</span><br> Have 1 year of experience working as a workshop manager in royal enfield.",
+                "date": "2020"
+},
+{
+    "logo": "./data/experience/robogenius.png",
+        "name": "Robogenius",
+            "description": "<span class='imp mb-10'>Profile : Python Trainee</span><br> Have 1 year of experience teaching python and Django in Robogenius.",
+                "date": "2021"
+},
